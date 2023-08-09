cd144670cdc034f217dbc7198e5a00c0317866c8
Henry Jacobs <henryjacobscode@gmail.com>
2023-08-09 14:47:15 -0400

    Add useFeature and useFlagStatus to index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import FlagProvider from "./FlagProvider"
 import useFlag from "./useFlag"
+import useFeature from "./useFeature"
+import useFlagStatus from "./useFlagStatus"
 
 export {
     FlagProvider, 
     useFlag, 
+    useFeature, 
+    useFlagStatus, 
 }
